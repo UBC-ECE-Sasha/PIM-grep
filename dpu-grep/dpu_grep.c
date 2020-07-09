91138bf016a51abfbdfaa46477c18f16d57ec5f8
Joel Nider <joel@ece.ubc.ca>
2020-07-09 23:43:30 +0200

    Remove unused variables
    Most of these were from the original grep, and are not needed. Some, because
    we do not implement that functionality yet, and some because we implement
    options in a different way (flags, not separate variables).
    
    Signed-off-by: Joel Nider <joel@ece.ubc.ca>

--- dpu-grep/dpu_grep.c
+++ dpu-grep/dpu_grep.c
@@ -12,30 +12,29 @@ extern uint32_t pattern_length;
 extern struct grep_options options;
 
 /* Flags controlling the style of output. */
-static enum
+/*static enum
 {
   BINARY_BINARY_FILES,
   TEXT_BINARY_FILES,
   WITHOUT_MATCH_BINARY_FILES
-} binary_files;		/* How to handle binary files.  */
+} binary_files;		 How to handle binary files.  */
 
 /* Options for output as a list of matching/non-matching files */
-static enum
+/*static enum
 {
   LISTFILES_NONE,
   LISTFILES_MATCHING,
   LISTFILES_NONMATCHING,
-} list_files;
+} list_files; */
 
 //static int filename_mask;	/* If zero, output nulls after filenames.  */
 //static bool out_quiet;		/* Suppress all normal output. */
-static bool out_invert;		/* Print nonmatching stuff. */
+//static bool out_invert;		/* Print nonmatching stuff. */
 //static bool out_line;		/* Print line numbers. */
 //static bool out_byte;		/* Print byte offsets. */
 //static intmax_t max_count;	/* Max number of selected
 //                                   lines from an input file.  */
 //static bool line_buffered;	/* Use line buffering.  */
-static char *label = NULL;      /* Fake filename for stdin */
 
 /* Internal variables to keep track of byte count, context, etc. */
 //static uintmax_t totalcc;	/* Total character count before bufbeg. */
@@ -43,7 +42,7 @@ static char *label = NULL;      /* Fake filename for stdin */
 //static char *lastout;		/* Pointer after last character output;
 //                                   NULL if no character has been output
 //                                   or if it's conceptually before bufbeg. */
-static intmax_t outleft;	/* Maximum number of selected lines.  */
+//static intmax_t outleft;	/* Maximum number of selected lines.  */
 //static intmax_t pending;	/* Pending lines of output.
 //                                   Always kept 0 if out_quiet is true.  */
 //static bool dev_null_output;	/* Stdout is known to be /dev/null.  */
@@ -59,7 +58,7 @@ static intmax_t outleft;	/* Maximum number of selected lines.  */
 /*static off_t after_last_match;	 Pointer after last matching line that
                                    would have been output if we were
                                    outputting characters. */
-static bool skip_empty_lines;	/* Skip empty lines in data.  */
+/*static bool skip_empty_lines;	 Skip empty lines in data.  */
 
 static unsigned char READ_BYTE(struct in_buffer_context *_i)
 {
@@ -69,22 +68,6 @@ static unsigned char READ_BYTE(struct in_buffer_context *_i)
 	return ret;
 }
 
-/* Return true if BUF, of size SIZE, has a null byte.
-   BUF must be followed by at least one byte,
-   which may be arbitrarily written to or read from.  */
-static bool
-buf_has_nulls (char *buf, size_t size)
-{
-  buf[size] = 0;
-  return strlen (buf) != size;
-}
-
-static size_t execute (void *vdc, char const *buf, size_t size, size_t *match_size,
-           char const *start_ptr)
-{
-	return 0;
-}
-
 /* Search a given (non-directory) file.  Return a count of lines printed.
    Set *INEOF to true if end-of-file reached.  */
 uint32_t grep(struct in_buffer_context *buf, uint32_t *line_count)
@@ -92,9 +75,10 @@ uint32_t grep(struct in_buffer_context *buf, uint32_t *line_count)
 	uint8_t p_index = 0;
 	uint32_t match_count = 0;
 	uint32_t prev_match_line = -1;
+#ifdef DEBUG
 	uint8_t task_id = me();
+#endif // DEBUG
 
-	dbg_printf("[%i] %u term=%s length=%u lines=%u\n", task_id, buf->length, pattern, pattern_length, *line_count);
 	for (uint32_t i=0; i < buf->length; i++)
 	{
 		char c = READ_BYTE(buf);
